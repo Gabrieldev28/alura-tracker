615288d84e4c178f3e7e4388e0c3db9a35ddc752
Gabrieldev28 <gabrieldev28@gmail.com>
2026-04-16 17:09:00 -0300

    projeto iniciado

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import SideBar from './components/SideBar.vue';
+
+</script>
+
+<template>
+  <main class="w-full flex">
+    <div class="w-1/4 ">
+      <SideBar />
+
+    </div>
+    <div class="w-3/4 bg-cyan-100">
+      <span><p>1</p></span>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
